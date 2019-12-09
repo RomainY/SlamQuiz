5006e352929cb339165b7e9b1c24f8a5dbbd0cb0
yverrom <yverrom@ndlp.fr>
2019-12-09 12:55:29 +0100

    Question (pas fini)

---
+++ src/Form/QuestionType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Question;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('categories')
+            ->add('answer')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Question::class,
+        ]);
+    }
+}
